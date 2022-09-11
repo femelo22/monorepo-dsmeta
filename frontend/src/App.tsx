286f698800e06057b1352e0c9da6bba1ba447506
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2022-09-11 19:18:15 -0300

    feat: add toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { ToastContainer } from "react-toastify";
 import Header from "./components/Header";
-import { NotificationButton } from "./components/NotificationButton";
 import SalesCard from "./components/SalesCard";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+      <ToastContainer /> 
       <Header />
       <main>
         <section id="sales">
